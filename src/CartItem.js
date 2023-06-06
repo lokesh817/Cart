4df2dc562cc3007a2ff6c1876ed4dd29d773ea5c
lokesh817 <lokeshkumarparihar143@gmail.com>
2023-06-06 07:28:51 +0530

    increase qty method added

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,20 +1,43 @@
 import React from "react";
-class CartIten extends React.Component{
+class CartItem extends React.Component{
+    constructor () {
+        super();
+        this.state = {
+          price: 999,
+          title: 'Mobile Phone',
+          qty: 1,
+          img: ''
+        }
+        
+    }
+    increaseQty = ()=>{
+        // console.log("Current QUantity:",this.state.qty);
+        this.state.qty=this.state.qty+1;
+    }
     render(){
+        const { price, title, qty } = this.state;
         return(
-            <div className= "Cart-item">
+            <div className= "cart-item">
                 <div className= "left-block">
                     <img style={style.image}/>
                 </div>
                 <div className="right-block">
-                    <div style={{fontSize:25}}>Phone</div>
-                    <div style={{color:'#777'}}>Rs 999</div>
-                    <div style={{color:'#777'}}>Qty: 1</div>
-                    <div className="Cart-item-actions">  </div>
+                    <div style={{fontSize:25}}>{title}</div>
+                    <div style={{color:'#777'}}>Rs {price}</div>
+                    <div style={{color:'#777'}}>Qty: {qty}</div>
+                    <div className="cart-item-actions">  
+                    {/* {Buttons} */}
+                        <img 
+                            alt="increase" 
+                            className="action-icons" 
+                            src="https://cdn-icons-png.flaticon.com/128/3303/3303893.png"
+                            onClick={this.increaseQty}    
+                        />
+                        <img alt="decrease" className="action-icons" src="https://cdn-icons-png.flaticon.com/128/1828/1828906.png" />
+                        <img alt="delete" className="action-icons" src="https://cdn-icons-png.flaticon.com/128/484/484611.png" />
+                    </div>
                 </div>
-
             </div>
-            
         );
     }
 }
@@ -26,4 +49,4 @@ const style={
         background:'#ccc'
     }
 }
-export default CartIten;
+export default CartItem;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+// import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// ReactDOM.render(<App />, document.getElementById('root'));
+// createRoot.render(, document.getElementById('root'));
+let root=createRoot(document.getElementById('root'));
+root.render(<App tab="home"/>)
 
